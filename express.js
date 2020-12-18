1bd91dca1a8c605e8543e7ab3dd63b95ca094e44
amirzaid <amirzaid27@gmail.com>
2020-12-18 18:44:01 +0200

    add heroku redirect uri

--- express.js
+++ express.js
@@ -28,7 +28,7 @@ const server = http.createServer(app); // Create a server
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = "/callback"; // Your redirect uri
+var redirect_uri = "http://spotify--jukebox.herokuapp.com/callback"; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
